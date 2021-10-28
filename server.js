1d6df783727f9cdc5e68736aec699383c17acd9f
arunkumar141997 <arun@higheredlab.com>
2021-10-28 13:14:46 +0530

    Updated files

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 const fs = require('fs')
 const express = require('express');
 require('dotenv').config()
-const PORT = 8080;
+const PORT = 3000;
 const BASE_URL = process.env.BASE_URL
 
 const app = express();
-app.get('/api/v1/analytics/', async (req, res) => {
+app.use(express.json({limit: '1mb'}));
+app.use(express.urlencoded({extended: true,limit: '1mb'}));
+app.use(express.static('public'));
+
+app.post('/api/v1/analytics/', async (req, res) => {
   let data = {}
-  console.log(req)
+  let meetingId, language, token
+  meetingId = req.body.meeting_id
+  language = req.body.language
   try {
-    const token = await fs.readdirSync(`/root/ana/learning-dashboard/${req.params.meetingID}`)
-    data["url"] = `${BASE_URL}/learning-dashboard/?meeting=${req.params.meetingID}&report=${token}`
-    await  res.json(data);
-  } catch (e) {
-    res.sendStatus(404);
-  }
-});
+     token = await fs.readdirSync(`/home/dev/ana/learning-dashboard/${meetingId}`)
+     data["analytics_url"] = `${BASE_URL}/learning-dashboard/?meeting=${meetingId}&report=${token}&lang=${language}`
+     await  res.json(data);
+    } catch (e) {
+      res.sendStatus(404);
+    }
+  });
+
+app.get('/api/v1/check',  (req, res) => {
+   res.json({'status': 'ok'})
+})
 
 app.listen(PORT, () => {
     console.log("Server running on port 3000");
