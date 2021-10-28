42dc0f1ddac4dcad819f3df9179f7a28afdf18ec
arunkumar141997 <arun@higheredlab.com>
2021-10-28 06:50:35 +0000

    Updated .env

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const fs = require('fs')
 const express = require('express');
 require('dotenv').config()
 const PORT = 8080;
+const BASE_URL = process.env.BASE_URL
 
 const app = express();
-app.get('/:meetingID', async (req, res) => {
+app.get('/api/v1/analytics/', async (req, res) => {
+  let data = {}
+  console.log(req)
   try {
-    const files = await fs.readdirSync(`/usr/src/app/learning-dashboard/${req.params.meetingID}`)
-    await  res.send(files);
+    const token = await fs.readdirSync(`/root/ana/learning-dashboard/${req.params.meetingID}`)
+    data["url"] = `${BASE_URL}/learning-dashboard/?meeting=${req.params.meetingID}&report=${token}`
+    await  res.json(data);
   } catch (e) {
     res.sendStatus(404);
   }
